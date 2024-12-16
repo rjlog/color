204d16ac8dac5633af0b288afad0bae928b48658
Loedje <loedje5@gmail.com>
2024-12-16 12:04:31 +0100

    Added initializePositions()

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector('#gl-canvas');
 const canvasHeight = canvas.clientHeight;
 const canvasWidth = canvas.clientWidth;
-const gl = canvas.getContext('webgl', { antialias: false });
+const gl = canvas.getContext('webgl2', { antialias: false });
 const vertexShaderSource = document.querySelector('#vertex-shader-2d').text;
 const fragmentShaderSource = document.querySelector('#fragment-shader-2d').text;
 
 main();
+
 function main() {
   if (!gl) {
     alert('Unable to initialize WebGL. Your browser or machine may not support it.');
@@ -18,29 +19,10 @@ function main() {
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
   const program = createProgram(gl, vertexShader, fragmentShader);
   
+  initializePositions(program);
   const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
-  const positionAttributeLocation = gl.getAttribLocation(program, 'position');
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      0.0, 0.0, 
-      1.0, 0.0, 
-      0.0, 1.0, 
-      
-      1.0, 0.0,
-      1.0, 1.0, 
-      0.0, 1.0,
-    ]), gl.STATIC_DRAW);
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  const size = 2; // 2 components per iteration
-  const type = gl.FLOAT;
-  const normalize = false;
-  const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  const offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-  gl.enableVertexAttribArray(positionAttributeLocation);
+  
+  
   gl.useProgram(program);
 
   const draw = () => drawScene(resolutionUniformLocation);
@@ -49,11 +31,11 @@ function main() {
 }
 
 function drawScene(resolutionUniformLocation) {
-  gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
-
   canvas.width = Math.floor(canvasWidth * window.devicePixelRatio);
   canvas.height = Math.floor(canvasHeight * window.devicePixelRatio);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  
+  gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
   
   const primitiveType = gl.TRIANGLES;
   const offset = 0;
@@ -63,6 +45,29 @@ function drawScene(resolutionUniformLocation) {
   // Scale down to original size
   canvas.style.width = canvasWidth + 'px';
   canvas.style.height = canvasHeight + 'px';
+}
+
+function initializePositions(program) {
+  const positionAttributeLocation = gl.getAttribLocation(program, 'position');
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        0.0, 0.0, 
+        1.0, 0.0, 
+        0.0, 1.0,
+
+        1.0, 0.0, 
+        1.0, 1.0, 
+        0.0, 1.0,
+      ]), gl.STATIC_DRAW);
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  const size = 2; // 2 components per iteration
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  let offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+  gl.enableVertexAttribArray(positionAttributeLocation);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
